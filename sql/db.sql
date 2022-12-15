7ba30f165b273e400f054c08343e2e2348c6fc2c
Francesco <lazzarellifrancesco@gmail.com>
2022-12-15 17:59:26 +0100

    modificato db e sistemato footer

--- sql/db.sql
+++ sql/db.sql
@@ -3,56 +3,101 @@ create schema BloodBankPisa default character set utf8;
 use BloodBankPisa;
 
 set foreign_key_checks = 0; -- to remove fk check
-set global event_scheduler = on; -- start scheduler event
+-- set global event_scheduler = on; -- start scheduler event
+
+-- aggiungere cascade
 
 --  ----------------- --
 --  USER              --
 -- ------------------ --
 
-create table if not exists `user` {
+create table if not exists `admin` {
     `_id` int not null auto_increment,
     `first_name` varchar(30) not null,
     `second_name` varchar(30) not null,
-    `type` tinyint not null check (`type` in (0,1,2, 3)), -- 0 super admin, 1 admin, 2 donator, 3 applicant
+    `type` tinyint not null check (`type` in (0,1)), -- 0 super admin, 1 admin, 2 donator, 3 applicant
+    `email` varchar(50) not null,
+    `hash_pwd` tinytext not null,
+    primary key (`_id`)
+} engine = InnoDB;
+
+create table if not exists `donator` {
+    `_id` int not null auto_increment,
+    `first_name` varchar(30) not null,
+    `second_name` varchar(30) not null,
     `email` varchar(50) not null,
     `country` varchar(20) not null,
     `city` varchar(40) not null,
     `phone` varchar(11) not null,
     `hash_pwd` tinytext not null,
-    `auth` tinyint not null check (`auth` in (0,1)), -- 0 not enabled, 1 enable to use account
+    `blood_group` varchar(3) not null,
+    `isAuth` tinyint not null check (`auth` in (0,1)), -- 0 not enabled, 1 enable to use account
+    primary key (`_id`)
+} engine = InnoDB;
+
+create table if not exists `patient` {
+    `_id` int not null auto_increment,
+    `first_name` varchar(30) not null,
+    `second_name` varchar(30) not null,
+    `email` varchar(50) not null,
+    `country` varchar(20) not null,
+    `city` varchar(40) not null,
+    `phone` varchar(11) not null,
+    `hash_pwd` tinytext not null,
+    `blood_group` varchar(3) not null,
+    `why` text not null,  -- why need blood trasfusion
+    `isAuth` tinyint not null check (`auth` in (0,1)), -- 0 not enabled, 1 enable to use account
     primary key (`_id`)
 } engine = InnoDB;
 
 --  ----------------- --
---  NEWS              --
+--  SITE              --
 -- ------------------ --
 
-create table if not exists `news` {
+create table if not exists `site` {
     `_id` int not null auto_increment,
-    `title` varchar(140) not null,
-    `body` text not null,
-    `img` tinytext default null,
-    `author_` int not null,
-    primary key (`_id`),
-    foreign key (`author_`) references `user` (`_id`)
-} engine = InnoDB;
-
-create index `index_author_1` ON `news` (`author_`);
+    `city` varchar(45) not null,
+    `nbs_Ap` int not null default 0, -- number os a+ blood sack -- gestire tutto dai trigger per aumentare e togliere
+    `nbs_Am` int not null default 0, -- number os a- blood sack
+    `nbs_Bp` int not null default 0, -- number os b+ blood sack
+    `nbs_Bm` int not null default 0, -- number os b- blood sack
+    `nbs_ABp` int not null default 0, -- number os ab+ blood sack
+    `nbs_ABm` int not null default 0, -- number os ab- blood sack
+    `nbs_Zp` int not null default 0, -- number os 0+ blood sack
+    `nbs_Zm` int not null default 0, -- number os 0- blood sack
+    primary key (`_id`)
+}
 
 --  ----------------- --
 --  REQUEST           --
 -- ------------------ --
 
-create table if not exists `request` {
+create table if not exists `blood_request` {
     `_id` int not null auto_increment,
-    `why` text default null, -- null = donator, not null = applicant
-    `pending` tinyint not null check (`auth` in (0,1)), -- 0 pending, 1 accepted
-    `users_` int not null,
+    `date` date default null,
+    `isPending` tinyint not null check (`auth` in (0,1)), -- 0 pending, 1 accepted
+    `patient_` int not null,
+    `site_` int not null,
     primary key (`_id`),
-    foreign key (`author_`) references `user` (`_id`)
+    foreign key (`patient_`) references `patient` (`_id`),
+    foreign key (`site_`) references `site` (`_id`)
 }
 
-create index `index_author_2` ON `request` (`author_`);
+create index `index_patient` ON `blood_request` (`patient_`);
+create index `index_site_1` ON `donation` (`_site_`);
+
+
+create table if not exists `donation` {
+    `_donator_` int not null,
+    `_site_` int not null,
+    `date` date int not null, --  data della donazione perchè non può donare entro tot mesi controllare ultima data e bloccare tasto dona
+    primary key (`_donator_`, `_site_`),
+    foreign key (`_donator_`) references `donator` (`_id`),
+    foreign key (`_site_`) references `site` (`_id`)
+}
+
+create index `index_donator` ON `donation` (`_donator_`);
+create index `index_site_1` ON `donation` (`_site_`);
 
 --  ----------------- --
 --  MESSAGE           --
@@ -67,3 +112,19 @@ create table if not exists `message` {
     `email` varchar(60) not null,
     primary key (`_id`)
 } engine = InnoDB;
+
+--  ----------------- --
+--  NEWS              --
+-- ------------------ --
+
+create table if not exists `news` {
+    `_id` int not null auto_increment,
+    `title` varchar(140) not null,
+    `body` text not null,
+    `img` tinytext default null,
+    `author_` int not null,
+    primary key (`_id`),
+    foreign key (`author_`) references `admin` (`_id`)
+} engine = InnoDB;
+
+create index `index_author` ON `news` (`author_`);
